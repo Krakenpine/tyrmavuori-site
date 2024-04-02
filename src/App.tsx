9cb5e29da6c5fc309577d46c7cbf59b6b965be75
Lauri Pekkarinen <lauri.pekkarinen@kapsi.fi>
2024-04-02 08:57:38 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo1.png';
+import { albums } from "./albums";
 import './App.css';
+import './Cardinal.ttf';
 
 function App() {
+  const [selectedAlbum, setSelectedAlbum] = useState(0);
+  const [menuSelection, setMenuSelection] = useState(0);
+  const [fadeOn, setFadeOn] = useState(false);
+
+  function handleButtonClick(index: number) {
+    if (index !== selectedAlbum) {
+      setMenuSelection(index);
+      setFadeOn(true);
+      setTimeout(() => {
+        setSelectedAlbum(index)
+        }, 401);
+      setTimeout(() => {
+        setFadeOn(false)
+        }, 400);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="Tyrmavuori-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Tyrmävuori
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-text">
+          Tyrmävuori tells a story of a goblin civilization in a series of intrumental concept albums.
+        </div>
+        <div className="streaming-links">
+          <a href="https://tyrmavuori.bandcamp.com/">Tyrmävuori on Bandcamp</a>
+          <div>Tyrmävuori on Spotify</div>
+        </div>
       </header>
+      <div className="menu">
+        {albums.map((album, index) => {
+          return <div
+            className={"menu-album-title" + (index === (menuSelection !== selectedAlbum ? menuSelection : selectedAlbum) ? " album-title-selected" : " album-title-not-selected")}
+            onClick={() => handleButtonClick(index)}>
+                <div className={"menu-album-title-text"}>{album.title}</div>
+              </div>
+        })}
+      </div>
+      <div className={"album-content " + (fadeOn ? "album-content-fading" : "album-content-appearing")}>
+        <img src={albums[selectedAlbum].cover} className="album-cover" alt=""/>
+        <div className="album-title-container">
+          <div className="album-title">{albums[selectedAlbum].title}</div>
+          
+          <div className="album-release-date">{albums[selectedAlbum].released ? "Released "+ albums[selectedAlbum].released : "Not yet released!"}</div>
+        </div>
+        <div className="album-tracklist">
+          {albums[selectedAlbum].tracks.map((track, index) => {
+            return <div className="single-track">
+              <div className="single-track-number">{index + 1}. </div>
+              <div className="single-track-name">{track}</div>
+              </div>
+            })}
+        </div>
+        <div className="vali" />
+        <div className="album-links">
+            {albums[selectedAlbum].links.map(link =>{
+              return <a href={link.link} className="album-link-single">{link.name}</a>
+            })}
+        </div>
+        <div className="vali" />
+        <div className="story">
+          {albums[selectedAlbum].story.map(storyLine => {return <div className="story-line">{storyLine}</div>})}
+        </div>
+      </div>
+      <hr className="hr-break"/>
+      <div className="credits">
+        <div>Composition, writing, and website by Lauri Pekkarinen</div>
+        <div>Story concepts by Pekka Rihko</div>
+        <div>Reconstitution cover art by <a href="https://www.mamuunudesign.com">Mamuunu Design</a></div>
+      </div>
     </div>
   );
 }
---
+++ src/albums.tsx
@@ -0,0 +1,65 @@
+import reconstitutionCover from "./reconstitution1.png";
+
+const reconstitutionTracks = [
+    "The Dark Lord has Fallen",
+    "Chaos and Confusion",
+    "Burning Villages",
+    "Desperate March",
+    "At the Gates of the Dungeon Mountain",
+    "No Gods, No Masters",
+    "Clans Unite!",
+    "Newfound Strength",
+    "Forging Mischief into Predators Guile"
+  ];
+
+  const reconstitutionStory = [
+    "The Dark Lord has been defeated by the forces of Light and lands tainted by evil are being purged. Those individuals belonging to \"higher\" races are being captured, tortured, and sentenced to death or life in hard labour in mock trials, while those considered to be of the \"evil\" races are killed on sight. Orcs and trolls have formed into small warbands, destroying, killing and eating everything they come across. Goblins tried to continue living in their villages, but are forced to flee when armies of Good come with swords and torches, acting only marginally more civilized than the orcs.",
+
+    "After millennia of slavery the goblins are confused by the situation. No one is telling them what to do, there is no army where all able-bodied are drafted and no taxes to be collected. The great arch-evils are not guiding them to a new land to be conquered. The goblins remember the old legends about the Dungeon Mountain where their forefathers dwelled. The most brave ones even whispered stories of an ancient sovereign goblin kingdom, where their kind once lived free.",
+    
+    "They collect their knowledge and the clan leaders send messengers to other goblin clans with crudely drawn maps that show location of the Dungeon Mountain and proposal to end quarrels between clans and meet there with truce. The goblins gather what is left of their belongings and embark on their journey.",
+    "Days turn into weeks, weeks turn into months, but finally they see the gargantuan mountain towering in the horizon. The weary travelers almost cannot believe the size and intricate carvings on the gates that hide a paved road that leads into the heart of the mountain.",
+    
+    "Within few weeks all clans that still exist have found their way to the Dungeon Mountain and elders of the clans have their first meeting. Old grudges are buried and the clans decide that it is time for the goblins to reclaim what is rightfully theirs. Human villagers living near the mountain feel the dread of something happening near them and goblin scouts bring news that there are rumours about monsters inhabiting the ancient mountain halls.",
+    
+    "The goblin elders take the news as a confirmation that their plan will succeed. No more will the goblins ever do bidding for other lords, it is time to change their mischievous nature into merciless determination and predators guile."
+];
+
+interface Album {
+    title: string,
+    released: string,
+    tracks: string[],
+    story: string[],
+    cover: string,
+    links: Link[]
+}
+
+interface Link {
+    name: string
+    link: string
+}
+
+const reconstitution: Album = {
+    title: "I. Reconstitution",
+    released: "12.4.2024",
+    tracks: reconstitutionTracks,
+    story: reconstitutionStory,
+    cover: reconstitutionCover,
+    links: [
+        { name: "Bandcamp", link: "https://tyrmavuori.bandcamp.com/album/i-reconstitution"},
+        { name: "Spotify", link: "ei ole"},
+        { name: "Youtube", link: "http://www.youtube.com"}
+    ]
+}
+
+export const albums: Album[] = [
+    reconstitution,
+    { title: "II.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "III.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "IV.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "V.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "VI.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "VII.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "VIII.", released: "", tracks: [], story: [], cover: "", links: [] },
+    { title: "IX.", released: "", tracks: [], story: [], cover: "", links: [] }
+]
